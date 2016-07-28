dd0b95f389c8d4dba09a76c22b053b0a61295682
Pavel Trutman <pavel.trutman@fel.cvut.cz>
2016-07-28 14:38:57 +0200

    Shuffling the correspondences in generated validating dataset.

--- deepag/bin/deepag/k_nn_pixels_generateData.m
+++ deepag/bin/deepag/k_nn_pixels_generateData.m
@@ -1,3 +1,7 @@
+% Script to generate syntetic data to k-NN testing in pixel space
+% 
+% Pavel Trutman
+% INRIA, 2016
 
 valSize = 500;
 overMarginCoef = 1.05;
@@ -5,6 +9,12 @@ overMarginCoef = 1.05;
 v = [repmat([-6/7; -4/7; -2/7; 0; 2/7; 4/7; 6/7], 2, 1); -repmat([-6/7; -4/7; -2/7; 0; 2/7; 4/7; 6/7], 2, 1)];
 
 u_tr = v;
-u_val = repmat(v, 1, valSize) + unifrnd(-1/7*overMarginCoef, 1/7*overMarginCoef, 28, valSize);
+u_val = zeros(28, valSize);
+for i = 1:valSize
+  u_val(:, i) = v + unifrnd(-1/7*overMarginCoef, 1/7*overMarginCoef, 28, 1);
+  % permute the correspondences
+  shuffle = randperm(7)';
+  u_val(:, i) = u_val([shuffle + 0; shuffle + 7; shuffle + 14; shuffle + 21], i);
+end
 
 save('pixels_syntetic.mat', 'u_tr', 'u_val', '-v7.3');
